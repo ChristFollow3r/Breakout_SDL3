621d940c07c8239eed6062d3f6e6fedd34549205
ChristFollw3r <nilbadia@gmail.com>
2026-03-28 12:16:14 +0100

    Added button class hpp.

---
+++ Breakout_C++/button.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "rectangle.hpp"
+
+class Button : public Rectangle {
+
+public:
+
+	SDL_Texture* texture;
+	SDL_FRect buttonTextRect;
+
+	Button(SDL_FRect rect, SDL_Renderer* renderer, SDL_Color color, SDL_Texture* texture, SDL_FRect buttonTextRect)
+	: Rectangle(rect, renderer, color), texture(texture), buttonTextRect(buttonTextRect) {};
+
+	bool Hovered();
+	bool Down();
+
+};
